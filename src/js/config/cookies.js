1db942cdca8a21b5df67e12f57b5626ec3cf6d64
HeroBuxx <me@herobuxx.me>
2024-09-18 21:43:54 +0700

    issues: Nuke JSCroot
    Makasih Pak Rolly
    
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- src/js/config/cookies.js
+++ src/js/config/cookies.js
@@ -1,3 +1,16 @@
-import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
 export let token = getCookie("token");
