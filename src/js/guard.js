a0a709f1a42fdaecd1ee39572f5d9a3268b1986a
HeroBuxx <me@herobuxx.me>
2024-11-17 03:31:16 +0700

    manage: Guard more stuff
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- src/js/guard.js
+++ src/js/guard.js
@@ -1,5 +1,12 @@
 import { token } from "./config/cookies.js";
+import { isCoreUser } from "./utils/myid.js";
+
+const isCore = await isCoreUser();
 
 if (token === "") {
     window.location.replace("http://scipio.hlcyn.co/login/");
+}
+
+if (!isCore) {
+    window.location.replace("/manage/unauthorized.html");
 }
