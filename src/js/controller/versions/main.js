d501ab9ad357641beca90baef9a2c0cd319e3c95
HeroBuxx <me@herobuxx.me>
2024-11-16 21:35:18 +0700

    manage: version: Create table view
    Signed-off-by: HeroBuxx <me@herobuxx.me>

---
+++ src/js/controller/versions/main.js
@@ -0,0 +1,74 @@
+import { versionURL } from "../../config/url.js";
+import { token } from "../../config/cookies.js";
+
+function showTeams(datas) {
+    const tableBody = document.getElementById('version-list');
+    tableBody.innerHTML = '';
+
+    datas.forEach(post => {
+        const card = document.createElement('tr');
+        card.classList.add('border-b', 'table-text');
+
+        card.innerHTML = `
+        <td scope="row" class="px-6 py-4 font-medium whitespace-nowrap text-white">
+            ${post.codename}
+        </td>
+        <td class="px-6 py-4">
+            ${post.branch}
+        </td>
+        <td class="px-6 py-4">
+            ${post.android_version}
+        </td>
+        `;
+        tableBody.appendChild(card);
+    });
+}
+
+function getTeams() {
+    var requestOptions = {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'Content-Type': 'application/json',
+        },
+        redirect: 'follow'
+    };
+
+    fetch(versionURL, requestOptions)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Something went wrong!');
+            }
+        })
+        .then(data => {
+            showTeams(data.data);
+            const spinner = document.getElementById('spinner');
+            const container = document.querySelector('.container');
+            spinner.classList.add('fade-out');
+            spinner.addEventListener('animationend', () => {
+                spinner.style.display = 'none';
+                container.removeAttribute('hidden');
+                container.classList.add('fade-in');
+            });
+        })
+        .catch(error => {
+            console.error('Fetch Error:', error);
+            const spinner = document.getElementById('spinner');
+            spinner.classList.add('fade-out');
+            spinner.addEventListener('animationend', () => {
+                spinner.style.display = 'none';
+                const container = document.querySelector('.container');
+                container.removeAttribute('hidden');
+                container.innerHTML = '<p class="text-white">Failed to load issues. Please try again later.</p>';
+                container.classList.add('fade-in');
+            });
+        });
+}
+
+function initialize() {
+    getTeams();
+}
+
+window.onload = initialize;
