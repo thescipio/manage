9f93f11848ba27203598104930498de03f8060f4
nekowawolf <irukainu20@gmail.com>
2024-12-01 16:58:47 +0700

    manage: add actions and delete confirmation modal

--- src/js/controller/versions/main.js
+++ src/js/controller/versions/main.js
@@ -20,6 +20,25 @@ function showTeams(datas) {
         <td class="px-6 py-4">
             ${post.android_version}
         </td>
+        <td class="px-4 py-4 text-sm whitespace-nowrap relative">
+                <button class="px-1 py-1 text duration-200 rounded-lg focus:outline-none dropdown-toggle" onclick="toggleDropdown(event)">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+                    </svg>
+                </button>
+               <div class="dropdown-menu hidden z-10 absolute w-15 top-3 sm:top-2 transform translate-x-4 -left-24">
+                <ul class="py-2 text-sm text button-lock flex justify-between divide-x">
+                    <a href="edit-paid.html?airdropId=#EDIT#" class="w-full">
+                    <li class="flex items-center px-4 py-2 cursor-pointer w-full action-color ">
+                        <i class="fa-solid fa-pen text-white"></i>
+                    </li>
+                    </a>
+                    <li id="deleteButton" class="flex items-center px-4 py-2 cursor-pointer w-full action-color " onclick="confirmDelete('#DELETE#')">
+                        <i class="fa-solid fa-trash text-white"></i> 
+                    </li>
+                </ul>
+            </div>
+        </td>
         `;
         tableBody.appendChild(card);
     });
